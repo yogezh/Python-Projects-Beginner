8310e617bc692a22286f3fc329e7ed00d1763a5d
yogezh <114815915+yogezh@users.noreply.github.com>
2023-01-06 11:54:12 +0530

    Create Palindrome.py
    Checking if the user's entered value is equal to it's reverse number or not that is palindrome or not.

---
+++ Palindrome.py
@@ -0,0 +1,15 @@
+print("::: LETS CHECK IF THE ENTERED NUMBER IS PALINDROME OR NOT :::")
+print(" :: YOU DON'T KNOW PALINDROME? K, LET ME EXPLAIN, IF USER'S VALUE AND REVERSE OF THAT VALUE ARE SAME THEN IT'S CALLED PALINROME. K? GOOD :) ")
+UserInput = int(input("Enter your value : "))
+reverse = 0
+Palindrome = UserInput
+while (UserInput>0) :
+    remainder = UserInput % 10
+    reverse = reverse * 10 + remainder
+    quotient = UserInput // 10
+    UserInput = quotient
+print(UserInput,reverse)
+if (Palindrome == reverse):
+    print("IT'S A PALINDROME AS"," Your value : ",Palindrome, " is equal to : ", reverse)
+else:
+    print("IT'S NOT A PALINDROME AS "," Your value : ",Palindrome, " is not equal to : ", reverse)
