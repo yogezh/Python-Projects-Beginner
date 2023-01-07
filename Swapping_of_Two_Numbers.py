7d4833c612a01bcdc915bb6dece75d2e14f8dcab
yogezh <114815915+yogezh@users.noreply.github.com>
2023-01-07 08:48:21 +0530

    Create Swapping_of_Two_Numbers.py
    Swapping the values of two numbers using a temporary variable

---
+++ Swapping_of_Two_Numbers.py
@@ -0,0 +1,12 @@
+num1 = int(input("Enter your First Number: "))
+num2 = int(input("Enter your Second Number: "))
+print("VALUES BEFORE SWAP:")
+print("Value of First Number is: %d"%num1)
+print("Value of Second Number is: %d"%num2)
+print("************************************")
+temp=num1
+num1=num2
+num2=temp
+print("VALUES AFTER SWAPPING")
+print("Value of First Number is: %d"%num1)
+print("Value of Second Number is: %d"%num2)
